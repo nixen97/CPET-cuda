0b3559468b14b7ed64a684a1b60143c6b723d44c
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-03-06 12:28:29 +0100

    IT'S ALIVE!

--- runTest.sh
+++ runTest.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+cd ext
+make
+cd ..
 python setup.py install
 nosetests tests/tests.py
--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ from glob import glob
 
 ext = Extension('ext',
                 language="c",
-                sources = glob('./ext/src/**.c', recursive=True))
+                sources = glob('./ext/py/**.c', recursive=True),
+                include_dirs=["./ext/src"],
+                library_dirs=["/usr/local/cuda/lib64"],
+                libraries=["cudart"],
+                extra_objects=["./build/test/bin/libLib."])
 
 setup(name = "ext",
       version = "1.0",
